bdf8441e94754ffa7b53ce167337858c517c21e8
ARs <estetiki@mail.ru>
2025-04-13 21:44:23 +0300

    added logging

--- utils/main_api.py
+++ utils/main_api.py
@@ -1,5 +1,7 @@
 import requests
 import json
+from utils.logger import Logger
+
 
 class Main_api:
     """Тут будут методы написанные специально для теста"""
@@ -7,7 +9,9 @@ class Main_api:
     @staticmethod
     # метод GET
     def get_method(url):
+        Logger.add_request(url, method="GET")
         result = requests.get(url)
+        Logger.add_response(result)
         return result
 
     """проверка статус кода"""
